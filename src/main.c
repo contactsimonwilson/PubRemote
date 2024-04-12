3f9e8715b2c63e4f357e255060deb467f63a8b51
Syler Clayton <2224238+Relys@users.noreply.github.com>
2024-04-12 00:04:15 -0700

    Added basic telemetry support. (#2)

--- src/main.c
+++ src/main.c
@@ -35,7 +35,8 @@
 
 static const char *TAG = "PUBMOTE-MAIN";
 
-uint8_t PEER_MAC_ADDRESS[6] = {72, 49, 183, 171, 63, 137};
+uint8_t PEER_MAC_ADDRESS[6] = {72, 49, 183, 171, 63, 137}; // Siwoz
+// uint8_t PEER_MAC_ADDRESS[6] = {60, 233, 14, 66, 213, 197};//Syler
 int64_t LAST_COMMAND_TIME = 0;
 
 led_strip_handle_t configure_led(void) {
@@ -94,8 +95,7 @@ void app_main(void) {
   esp_now_peer_info_t peerInfo = {};
   peerInfo.channel = 1; // Set the channel number (0-14)
   peerInfo.encrypt = false;
-  uint8_t PEER_MAC_ADDRESS2[6] = {72, 49, 183, 171, 63, 137};
-  memcpy(peerInfo.peer_addr, PEER_MAC_ADDRESS2, sizeof(PEER_MAC_ADDRESS2));
+  memcpy(peerInfo.peer_addr, PEER_MAC_ADDRESS, sizeof(PEER_MAC_ADDRESS));
   ESP_ERROR_CHECK(esp_now_add_peer(&peerInfo));
   // Log MAC address
   ESP_LOGI(TAG, "MAC Address: %02X:%02X:%02X:%02X:%02X:%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
