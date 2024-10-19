8bbfff0627f80d59a1d16fd8e246e3a282b74eb0
contactsimonwilson <contactsimonwilson@gmail.com>
2024-10-20 10:23:58 +1300

    add generic functions for handling NVS

--- src/remote/settings.c
+++ src/remote/settings.c
@@ -86,6 +86,7 @@ esp_err_t init_nvs() {
 
 // Function to initialize settings object, reading from NVS
 esp_err_t init_settings() {
+  ESP_LOGI(TAG, "Initializing settings...");
   esp_err_t err = init_nvs();
   if (err != ESP_OK) {
     ESP_LOGE(TAG, "Error initializing NVS!");
@@ -128,8 +129,7 @@ esp_err_t init_settings() {
   return ESP_OK;
 }
 
-// Function to write an integer to NVS
-esp_err_t nvs_write_int(const char *key, int32_t value) {
+static esp_err_t nvs_write(const char *key, void *value, nvs_type_t type, size_t length) {
   nvs_handle_t my_handle;
   esp_err_t err = nvs_open(STORAGE_NAMESPACE, NVS_READWRITE, &my_handle);
   if (err != ESP_OK) {
@@ -137,7 +137,41 @@ esp_err_t nvs_write_int(const char *key, int32_t value) {
     return err;
   }
 
-  err = nvs_set_i32(my_handle, key, value);
+  switch (type) {
+  case NVS_TYPE_I8:
+    err = nvs_set_i8(my_handle, key, *(int8_t *)value);
+    break;
+  case NVS_TYPE_U8:
+    err = nvs_set_u8(my_handle, key, *(uint8_t *)value);
+    break;
+  case NVS_TYPE_I16:
+    err = nvs_set_i16(my_handle, key, *(int16_t *)value);
+    break;
+  case NVS_TYPE_U16:
+    err = nvs_set_u16(my_handle, key, *(uint16_t *)value);
+    break;
+  case NVS_TYPE_I32:
+    err = nvs_set_i32(my_handle, key, *(int32_t *)value);
+    break;
+  case NVS_TYPE_U32:
+    err = nvs_set_u32(my_handle, key, *(uint32_t *)value);
+    break;
+  case NVS_TYPE_I64:
+    err = nvs_set_i64(my_handle, key, *(int64_t *)value);
+    break;
+  case NVS_TYPE_U64:
+    err = nvs_set_u64(my_handle, key, *(uint64_t *)value);
+    break;
+  case NVS_TYPE_STR:
+    err = nvs_set_str(my_handle, key, (char *)value);
+    break;
+  case NVS_TYPE_BLOB:
+    err = nvs_set_blob(my_handle, key, value, length);
+    break;
+  default:
+    ESP_LOGE(TAG, "Unsupported data type!");
+    err = ESP_ERR_INVALID_ARG;
+  }
 
   if (err != ESP_OK) {
     ESP_LOGI(TAG, "Write done");
@@ -158,8 +192,8 @@ esp_err_t nvs_write_int(const char *key, int32_t value) {
   return err;
 }
 
-// Function to read an integer from NVS
-esp_err_t nvs_read_int(const char *key, int32_t *value) {
+// Function to read from NVS
+static esp_err_t nvs_read(const char *key, int32_t *value, nvs_type_t type, size_t length) {
   nvs_handle_t my_handle;
   esp_err_t err = nvs_open(STORAGE_NAMESPACE, NVS_READONLY, &my_handle);
   if (err != ESP_OK) {
@@ -167,10 +201,87 @@ esp_err_t nvs_read_int(const char *key, int32_t *value) {
     return err;
   }
 
-  err = nvs_get_i32(my_handle, key, value);
+  switch (type) {
+  case NVS_TYPE_I8:
+    err = nvs_get_i8(my_handle, key, (int8_t *)value);
+    if (err == ESP_OK) {
+      ESP_LOGI(TAG, "Read done, value = %d", *(int8_t *)value);
+    }
+    break;
+  case NVS_TYPE_U8:
+    err = nvs_get_u8(my_handle, key, (uint8_t *)value);
+    if (err == ESP_OK) {
+      ESP_LOGI(TAG, "Read done, value = %u", *(uint8_t *)value);
+    }
+    break;
+  case NVS_TYPE_I16:
+    err = nvs_get_i16(my_handle, key, (int16_t *)value);
+    if (err == ESP_OK) {
+      ESP_LOGI(TAG, "Read done, value = %d", *(int16_t *)value);
+    }
+    break;
+  case NVS_TYPE_U16:
+    err = nvs_get_u16(my_handle, key, (uint16_t *)value);
+    if (err == ESP_OK) {
+      ESP_LOGI(TAG, "Read done, value = %u", *(uint16_t *)value);
+    }
+    break;
+  case NVS_TYPE_I32:
+    err = nvs_get_i32(my_handle, key, (int32_t *)value);
+    if (err == ESP_OK) {
+      ESP_LOGI(TAG, "Read done, value = %ld", *(int32_t *)value);
+    }
+    break;
+  case NVS_TYPE_U32:
+    err = nvs_get_u32(my_handle, key, (uint32_t *)value);
+    if (err == ESP_OK) {
+      ESP_LOGI(TAG, "Read done, value = %lu", *(uint32_t *)value);
+    }
+    break;
+  case NVS_TYPE_I64:
+    err = nvs_get_i64(my_handle, key, (int64_t *)value);
+    if (err == ESP_OK) {
+      ESP_LOGI(TAG, "Read done, value = %lld", *(int64_t *)value);
+    }
+    break;
+  case NVS_TYPE_U64:
+    err = nvs_get_u64(my_handle, key, (uint64_t *)value);
+    if (err == ESP_OK) {
+      ESP_LOGI(TAG, "Read done, value = %llu", *(uint64_t *)value);
+    }
+    break;
+  case NVS_TYPE_STR: {
+    size_t required_size;
+    // Get the size of the string first
+    err = nvs_get_str(my_handle, key, NULL, &required_size);
+    if (err == ESP_OK) {
+      char *str_value = (char *)malloc(required_size);
+      if (str_value == NULL) {
+        ESP_LOGE(TAG, "Memory allocation failed!");
+        err = ESP_ERR_NO_MEM;
+      }
+      else {
+        err = nvs_get_str(my_handle, key, str_value, &required_size);
+        if (err == ESP_OK) {
+          strcpy((char *)value, str_value);
+          ESP_LOGI(TAG, "Read done, value = %s", str_value);
+        }
+        free(str_value);
+      }
+    }
+    break;
+  }
+  case NVS_TYPE_BLOB:
+    err = nvs_get_blob(my_handle, key, value, &length);
+    break;
+  default:
+    ESP_LOGE(TAG, "Unsupported data type!");
+    err = ESP_ERR_INVALID_ARG;
+  }
+
   switch (err) {
   case ESP_OK:
-    ESP_LOGI(TAG, "Read done, value = %ld\n", *value);
+    ESP_LOGI(TAG, "Read done, value = %ld", *value);
     break;
   case ESP_ERR_NVS_NOT_FOUND:
     ESP_LOGE(TAG, "The value is not initialized yet!");
@@ -181,4 +292,24 @@ esp_err_t nvs_read_int(const char *key, int32_t *value) {
 
   nvs_close(my_handle);
   return err;
+}
+
+// Function to write an integer to NVS
+esp_err_t nvs_write_int(const char *key, int32_t value) {
+  return nvs_write(key, &value, NVS_TYPE_I32, 0);
+}
+
+// Function to write a blob to NVS
+esp_err_t nvs_write_blob(const char *key, void *value, size_t length) {
+  return nvs_write(key, &value, NVS_TYPE_BLOB, length);
+}
+
+// Function to read an integer from NVS
+esp_err_t nvs_read_int(const char *key, int32_t *value) {
+  return nvs_read(key, value, NVS_TYPE_I32, 0);
+}
+
+// Function to read a blob from NVS
+esp_err_t nvs_read_blob(const char *key, void *value, size_t length) {
+  return nvs_read(key, value, NVS_TYPE_BLOB, length);
 }
