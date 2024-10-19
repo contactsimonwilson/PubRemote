4aacd4c914c5ecf79be2cbd7a59efbedb18c7f6c
contactsimonwilson <contactsimonwilson@gmail.com>
2024-10-20 11:15:24 +1300

    Merge NVS changes with latest changes from master

--- src/remote/settings.c
+++ src/remote/settings.c
@@ -15,6 +15,7 @@ static const char *TAG = "PUBREMOTE-SETTINGS";
 #define EXPO_ADJUST_FACTOR 100
 
 static const AutoOffOptions DEFAULT_AUTO_OFF_TIME = AUTO_OFF_5_MINUTES;
+static const uint8_t DEFAULT_PEER_ADDR[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 
 DeviceSettings device_settings = {
     .bl_level = BL_LEVEL_DEFAULT,
@@ -66,47 +67,24 @@ void save_auto_off_time() {
 }
 
 esp_err_t save_pairing_data() {
-  nvs_handle_t my_handle;
-  esp_err_t err = nvs_open(STORAGE_NAMESPACE, NVS_READWRITE, &my_handle);
+  ESP_LOGI(TAG, "Saving pairing data...");
+  esp_err_t err = nvs_write_int("pairing_state", (int32_t)pairing_settings.state);
   if (err != ESP_OK) {
-    ESP_LOGE(TAG, "Error (%s) opening NVS handle!", esp_err_to_name(err));
+    ESP_LOGE(TAG, "Error saving pairing state!");
+    return err;
+  }
+  err = nvs_write_int("secret_code", pairing_settings.secret_code);
+  if (err != ESP_OK) {
+    ESP_LOGE(TAG, "Error saving secret code!");
     return err;
   }
 
-  // Save the 'state' field as an integer
-  err = nvs_set_i32(my_handle, "pairing_state", (int32_t)pairing_settings.state);
+  err = nvs_write_blob("remote_addr", pairing_settings.remote_addr, sizeof(pairing_settings.remote_addr));
   if (err != ESP_OK) {
-    ESP_LOGE(TAG, "Failed to write pairing state!");
-    nvs_close(my_handle);
+    ESP_LOGE(TAG, "Error saving remote address!");
     return err;
   }
 
-  // Save the 'secret_code' field
-  err = nvs_set_i32(my_handle, "secret_code", pairing_settings.secret_code);
-  if (err != ESP_OK) {
-    ESP_LOGE(TAG, "Failed to write secret code!");
-    nvs_close(my_handle);
-    return err;
-  }
-
-  // Save the 'remote_addr' array as a blob
-  err = nvs_set_blob(my_handle, "remote_addr", pairing_settings.remote_addr, sizeof(pairing_settings.remote_addr));
-  if (err != ESP_OK) {
-    ESP_LOGE(TAG, "Failed to write remote address!");
-    nvs_close(my_handle);
-    return err;
-  }
-
-  // Commit the changes
-  err = nvs_commit(my_handle);
-  if (err != ESP_OK) {
-    ESP_LOGE(TAG, "Failed to commit changes to NVS!");
-    nvs_close(my_handle);
-    return err;
-  }
-
-  // Close the NVS handle
-  nvs_close(my_handle);
   ESP_LOGI(TAG, "Pairing data saved successfully.");
   return ESP_OK;
 }
@@ -180,36 +158,27 @@ esp_err_t init_settings() {
   calibration_settings.expo = nvs_read_int("x_expo", &expo) == ESP_OK ? (float)(expo / EXPO_ADJUST_FACTOR) : STICK_EXPO;
 
   // Reading pairing settings
-  int32_t pairing_state = PAIRING_STATE_UNPAIRED;
-  nvs_read_int("pairing_state", &pairing_state);
-  pairing_settings.state = (PairingState)pairing_state;
+  pairing_settings.state =
+      nvs_read_int("deadband", &pairing_settings.state) == ESP_OK ? pairing_settings.state : PAIRING_STATE_UNPAIRED;
 
-  nvs_read_int("secret_code", &pairing_settings.secret_code) == ESP_OK ? pairing_settings.secret_code : -1;
+  pairing_settings.secret_code =
+      nvs_read_int("secret_code", &pairing_settings.secret_code) == ESP_OK ? pairing_settings.secret_code : -1;
 
-  // Read the 'remote_addr' blob
-  nvs_handle_t my_handle;
-  err = nvs_open(STORAGE_NAMESPACE, NVS_READONLY, &my_handle);
-  if (err != ESP_OK) {
-    ESP_LOGE(TAG, "Error (%s) opening NVS handle!", esp_err_to_name(err));
-    return err;
+  uint8_t remote_addr[6];
+  err = nvs_read_blob("remote_addr", &remote_addr, sizeof(remote_addr));
+  if (err == ESP_OK) {
+    memcpy(pairing_settings.remote_addr, remote_addr, sizeof(remote_addr));
   }
-
-  size_t remote_addr_size = sizeof(pairing_settings.remote_addr);
-  err = nvs_get_blob(my_handle, "remote_addr", pairing_settings.remote_addr, &remote_addr_size);
-  if (err != ESP_OK) {
-    ESP_LOGE(TAG, "Failed to read remote_addr. Using default.");
-    memcpy(pairing_settings.remote_addr, (uint8_t[]){0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF},
-           sizeof(pairing_settings.remote_addr));
+  else {
+    memcpy(pairing_settings.remote_addr, DEFAULT_PEER_ADDR, sizeof(DEFAULT_PEER_ADDR));
   }
-
-  nvs_close(my_handle);
 
   return ESP_OK;
 }
 
 static esp_err_t nvs_write(const char *key, void *value, nvs_type_t type, size_t length) {
-  nvs_handle_t my_handle;
-  esp_err_t err = nvs_open(STORAGE_NAMESPACE, NVS_READWRITE, &my_handle);
+  nvs_handle_t nvs_handle;
+  esp_err_t err = nvs_open(STORAGE_NAMESPACE, NVS_READWRITE, &nvs_handle);
   if (err != ESP_OK) {
     ESP_LOGE(TAG, "Error (%s) opening NVS handle!", esp_err_to_name(err));
     return err;
@@ -217,34 +186,34 @@ static esp_err_t nvs_write(const char *key, void *value, nvs_type_t type, size_t
 
   switch (type) {
   case NVS_TYPE_I8:
-    err = nvs_set_i8(my_handle, key, *(int8_t *)value);
+    err = nvs_set_i8(nvs_handle, key, *(int8_t *)value);
     break;
   case NVS_TYPE_U8:
-    err = nvs_set_u8(my_handle, key, *(uint8_t *)value);
+    err = nvs_set_u8(nvs_handle, key, *(uint8_t *)value);
     break;
   case NVS_TYPE_I16:
-    err = nvs_set_i16(my_handle, key, *(int16_t *)value);
+    err = nvs_set_i16(nvs_handle, key, *(int16_t *)value);
     break;
   case NVS_TYPE_U16:
-    err = nvs_set_u16(my_handle, key, *(uint16_t *)value);
+    err = nvs_set_u16(nvs_handle, key, *(uint16_t *)value);
     break;
   case NVS_TYPE_I32:
-    err = nvs_set_i32(my_handle, key, *(int32_t *)value);
+    err = nvs_set_i32(nvs_handle, key, *(int32_t *)value);
     break;
   case NVS_TYPE_U32:
-    err = nvs_set_u32(my_handle, key, *(uint32_t *)value);
+    err = nvs_set_u32(nvs_handle, key, *(uint32_t *)value);
     break;
   case NVS_TYPE_I64:
-    err = nvs_set_i64(my_handle, key, *(int64_t *)value);
+    err = nvs_set_i64(nvs_handle, key, *(int64_t *)value);
     break;
   case NVS_TYPE_U64:
-    err = nvs_set_u64(my_handle, key, *(uint64_t *)value);
+    err = nvs_set_u64(nvs_handle, key, *(uint64_t *)value);
     break;
   case NVS_TYPE_STR:
-    err = nvs_set_str(my_handle, key, (char *)value);
+    err = nvs_set_str(nvs_handle, key, (char *)value);
     break;
   case NVS_TYPE_BLOB:
-    err = nvs_set_blob(my_handle, key, value, length);
+    err = nvs_set_blob(nvs_handle, key, value, length);
     break;
   default:
     ESP_LOGE(TAG, "Unsupported data type!");
@@ -258,7 +227,7 @@ static esp_err_t nvs_write(const char *key, void *value, nvs_type_t type, size_t
     ESP_LOGE(TAG, "Failed to write!");
   }
 
-  err = nvs_commit(my_handle);
+  err = nvs_commit(nvs_handle);
   if (err != ESP_OK) {
     ESP_LOGI(TAG, "Commit done");
   }
@@ -266,14 +235,14 @@ static esp_err_t nvs_write(const char *key, void *value, nvs_type_t type, size_t
     ESP_LOGE(TAG, "Failed to commit!");
   }
 
-  nvs_close(my_handle);
+  nvs_close(nvs_handle);
   return err;
 }
 
 // Function to read from NVS
 static esp_err_t nvs_read(const char *key, int32_t *value, nvs_type_t type, size_t length) {
-  nvs_handle_t my_handle;
-  esp_err_t err = nvs_open(STORAGE_NAMESPACE, NVS_READONLY, &my_handle);
+  nvs_handle_t nvs_handle;
+  esp_err_t err = nvs_open(STORAGE_NAMESPACE, NVS_READONLY, &nvs_handle);
   if (err != ESP_OK) {
     ESP_LOGE(TAG, "Error (%s) opening NVS handle!", esp_err_to_name(err));
     return err;
@@ -281,49 +250,49 @@ static esp_err_t nvs_read(const char *key, int32_t *value, nvs_type_t type, size
 
   switch (type) {
   case NVS_TYPE_I8:
-    err = nvs_get_i8(my_handle, key, (int8_t *)value);
+    err = nvs_get_i8(nvs_handle, key, (int8_t *)value);
     if (err == ESP_OK) {
       ESP_LOGI(TAG, "Read done, value = %d", *(int8_t *)value);
     }
     break;
   case NVS_TYPE_U8:
-    err = nvs_get_u8(my_handle, key, (uint8_t *)value);
+    err = nvs_get_u8(nvs_handle, key, (uint8_t *)value);
     if (err == ESP_OK) {
       ESP_LOGI(TAG, "Read done, value = %u", *(uint8_t *)value);
     }
     break;
   case NVS_TYPE_I16:
-    err = nvs_get_i16(my_handle, key, (int16_t *)value);
+    err = nvs_get_i16(nvs_handle, key, (int16_t *)value);
     if (err == ESP_OK) {
       ESP_LOGI(TAG, "Read done, value = %d", *(int16_t *)value);
     }
     break;
   case NVS_TYPE_U16:
-    err = nvs_get_u16(my_handle, key, (uint16_t *)value);
+    err = nvs_get_u16(nvs_handle, key, (uint16_t *)value);
     if (err == ESP_OK) {
       ESP_LOGI(TAG, "Read done, value = %u", *(uint16_t *)value);
     }
     break;
   case NVS_TYPE_I32:
-    err = nvs_get_i32(my_handle, key, (int32_t *)value);
+    err = nvs_get_i32(nvs_handle, key, (int32_t *)value);
     if (err == ESP_OK) {
       ESP_LOGI(TAG, "Read done, value = %ld", *(int32_t *)value);
     }
     break;
   case NVS_TYPE_U32:
-    err = nvs_get_u32(my_handle, key, (uint32_t *)value);
+    err = nvs_get_u32(nvs_handle, key, (uint32_t *)value);
     if (err == ESP_OK) {
       ESP_LOGI(TAG, "Read done, value = %lu", *(uint32_t *)value);
     }
     break;
   case NVS_TYPE_I64:
-    err = nvs_get_i64(my_handle, key, (int64_t *)value);
+    err = nvs_get_i64(nvs_handle, key, (int64_t *)value);
     if (err == ESP_OK) {
       ESP_LOGI(TAG, "Read done, value = %lld", *(int64_t *)value);
     }
     break;
   case NVS_TYPE_U64:
-    err = nvs_get_u64(my_handle, key, (uint64_t *)value);
+    err = nvs_get_u64(nvs_handle, key, (uint64_t *)value);
     if (err == ESP_OK) {
       ESP_LOGI(TAG, "Read done, value = %llu", *(uint64_t *)value);
     }
@@ -331,7 +300,7 @@ static esp_err_t nvs_read(const char *key, int32_t *value, nvs_type_t type, size
   case NVS_TYPE_STR: {
     size_t required_size;
     // Get the size of the string first
-    err = nvs_get_str(my_handle, key, NULL, &required_size);
+    err = nvs_get_str(nvs_handle, key, NULL, &required_size);
     if (err == ESP_OK) {
       char *str_value = (char *)malloc(required_size);
       if (str_value == NULL) {
@@ -339,7 +308,7 @@ static esp_err_t nvs_read(const char *key, int32_t *value, nvs_type_t type, size
         err = ESP_ERR_NO_MEM;
       }
       else {
-        err = nvs_get_str(my_handle, key, str_value, &required_size);
+        err = nvs_get_str(nvs_handle, key, str_value, &required_size);
         if (err == ESP_OK) {
           strcpy((char *)value, str_value);
           ESP_LOGI(TAG, "Read done, value = %s", str_value);
@@ -350,7 +319,7 @@ static esp_err_t nvs_read(const char *key, int32_t *value, nvs_type_t type, size
     break;
   }
   case NVS_TYPE_BLOB:
-    err = nvs_get_blob(my_handle, key, value, &length);
+    err = nvs_get_blob(nvs_handle, key, value, &length);
     break;
   default:
     ESP_LOGE(TAG, "Unsupported data type!");
@@ -368,7 +337,7 @@ static esp_err_t nvs_read(const char *key, int32_t *value, nvs_type_t type, size
     ESP_LOGE(TAG, "Error (%s) reading!", esp_err_to_name(err));
   }
 
-  nvs_close(my_handle);
+  nvs_close(nvs_handle);
   return err;
 }
 
