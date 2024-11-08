8863567edce196e27f13ee2faec95e3e43cffde0
contactsimonwilson <contactsimonwilson@gmail.com>
2024-11-08 18:24:02 +1300

    Add doubled fonts

--- src/ui/ui.h
+++ src/ui/ui.h
@@ -169,6 +169,9 @@ LV_IMG_DECLARE(ui_img_icon_png);    // assets/icon.png
 LV_FONT_DECLARE(ui_font_Inter_14);
 LV_FONT_DECLARE(ui_font_Inter_Bold_14);
 LV_FONT_DECLARE(ui_font_Inter_Bold_48);
+LV_FONT_DECLARE(ui_font_Inter_Bold_28);
+LV_FONT_DECLARE(ui_font_Inter_28);
+LV_FONT_DECLARE(ui_font_Inter_Bold_96);
 
 // UI INIT
 void ui_init(void);
