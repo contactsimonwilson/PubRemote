dab790353b06497c649ee2b3daaf78d331b36105
Jared Harrison <jared@jarediscoding.com>
2025-04-03 16:38:10 -0400

    Simplify display type env flags

--- firmware/src/display/display_driver.c
+++ firmware/src/display/display_driver.c
@@ -4,7 +4,7 @@ static const char *TAG = "PUBREMOTE-DISPLAY-DRIVER";
 
 #if DISP_GC9A01
   #include "gc9a01/display_driver_gc9a01.h"
-#elif DISP_SH8601_CO5300 || DISP_SH8601 || DISP_CO5300
+#elif DISP_SH8601 || DISP_CO5300
   #include "sh8601/display_driver_sh8601.h"
 #elif DISP_ST7789
   #include "st7789/display_driver_st7789.h"
@@ -14,7 +14,7 @@ esp_err_t test_display_communication(esp_lcd_panel_io_handle_t io_handle) {
   ESP_LOGI(TAG, "Testing display communication");
 #if DISP_GC9A01
   return gc9a01_test_display_communication(io_handle);
-#elif DISP_SH8601_CO5300 || DISP_SH8601 || DISP_CO5300
+#elif DISP_SH8601 || DISP_CO5300
   return sh8601_test_display_communication(io_handle);
 #elif DISP_ST7789
   return st7789_test_display_communication(io_handle);
@@ -25,7 +25,7 @@ esp_err_t display_driver_preinit() {
   ESP_LOGI(TAG, "Preinit display driver");
 #if DISP_GC9A01
   return gc9a01_display_driver_preinit();
-#elif DISP_SH8601_CO5300 || DISP_SH8601 || DISP_CO5300
+#elif DISP_SH8601 || DISP_CO5300
   return sh8601_display_driver_preinit();
 #elif DISP_ST7789
   return st7789_display_driver_preinit();
@@ -36,7 +36,7 @@ esp_err_t set_display_brightness(esp_lcd_panel_io_handle_t io_handle, uint8_t br
   ESP_LOGI(TAG, "Setting display brightness");
 #if DISP_GC9A01
   return gc9a01_set_display_brightness(io_handle, brightness);
-#elif DISP_SH8601_CO5300 || DISP_SH8601 || DISP_CO5300
+#elif DISP_SH8601 || DISP_CO5300
   return sh8601_set_display_brightness(io_handle, brightness);
 #elif DISP_ST7789
   return st7789_set_display_brightness(io_handle, brightness);
