286e1043f40bfe204e786bcb2aec1942b6e1abc2
contactsimonwilson <contactsimonwilson@gmail.com>
2025-07-23 22:58:09 +1200

    optimize control and transmit loop frequencies and waits

--- firmware/src/remote/peers.h
+++ firmware/src/remote/peers.h
@@ -2,8 +2,6 @@
 #define __PEERS_H
 #include <stdio.h>
 
-extern int64_t LAST_COMMAND_TIME;
-
 typedef struct {
   uint8_t mac[6];  // MAC address storage
   char name[32];   // Device name (adjust size as needed)
